b1b411d0f87caa4e11d60a30295d249007aef809
Mr.jianfei <444128852@qq.com>
2018-10-26 19:52:08 +0800

    test small button wave effect

--- src/views/VueBox.jsx
+++ src/views/VueBox.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import styled from "styled-components"
+// button wave effect
+import ButtonWaveEffect from "@src/utils/ButtonWaveEffect"
+const TestButton = styled.button`
+    width:30px;
+    height:30px;
+    line-height:30px;
+    background-color:purple;
+    border-radius:50%;
+    color:#fff;
+    position:relative;
+`;
 class VueBox extends React.Component {
+    ButtonWave = new ButtonWaveEffect()
+    testClick = e => {
+        this.ButtonWave.showWave(e)
+    }
     render() {
         return (
             <div className="container vue-box">
                 <p>Klay-Thompson</p>
+                <TestButton >x</TestButton>
             </div>
         )
     }
