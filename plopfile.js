109222dea42fbc29e94e102e5d50f45e37b7ac97
JianFei.Wang <jiucheng0605@gmail.com>
2019-10-22 14:32:04 +0800

    更新plopfile

--- plopfile.js
+++ plopfile.js
@@ -2,8 +2,7 @@
 const genComponent = require('./plop/generators/component')
 // 自动生成redux到对应目录
 const genModule = require('./plop/generators/module')
-// 可继续
-
+// 可继续扩展
 module.exports = function (plop) {
   // redux module generator
   plop.setGenerator('module', genModule())
