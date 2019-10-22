6697b1e969c440a736210cd7962017e02893230c
JianFei.Wang <jiucheng0605@gmail.com>
2019-10-22 17:26:02 +0800

    fix(loadable-component): 替换组件懒加载
    loadable-component instead of react-loadable

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,74 +1,64 @@
 /*
- *  react-loadable: load Async component
- *  Loadable(loader,loading) 
- *  loader: load component
- *  loading: default loading effect
+ *  load Async component
+ *  https://github.com/smooth-code/loadable-components
+ *  Arguments                Description
+ *  loadFn                 The function call to load the component.
+ *  options                Optional options.
+ *  options.fallback       Fallback displayed during the loading.
+ *  options.ssr            If false, it will not be processed server-side. Default to true.
+ *  options.cacheKey       Cache key function (see dynamic import)
  */
-import Loadable from "react-loadable"
-import Loading from "../components/Loading"
+import Loadable from '@loadable/component'
+// import Loading from '../components/Loading'
 
-const DashboardHome = Loadable({
-    loader: () => import("../views/dashboard"),
-    loading: Loading,
-})
+const DashboardHome = Loadable(() => import('../views/dashboard'))
 
-const ProfileHome = Loadable({
-    loader: () => import("../views/profile"),
-    loading: Loading,
-})
+const ProfileHome = Loadable(() => import('../views/profile'))
 
-const TableHome = Loadable({
-    loader: () => import("../views/table"),
-    loading: Loading,
-})
+const TableHome = Loadable(() => import('../views/table'))
 
-const ChartsHome = Loadable({
-    loader: () => import("../views/charts"),
-    loading: Loading,
-})
+const ChartsHome = Loadable(() => import('../views/charts'))
 
-const NoticeHome = Loadable({
-    loader: () => import("../views/notice"),
-    loading: Loading,
-})
+const NoticeHome = Loadable(() => import('../views/notice'))
+
 const routeList = [
     {
-        path: "/home/dashboard",
+        path: '/home/dashboard',
         exact: true,
-        icon: "dashboard",
-        sidebarName: "Dashboard",
+        icon: 'dashboard',
+        sidebarName: 'Dashboard',
         component: DashboardHome,
         permission: 4,
     },
     {
-        path: "/home/profile",
+        path: '/home/profile',
         exact: true,
-        icon: "profile",
-        sidebarName: "User Profile",
+        icon: 'profile',
+        sidebarName: 'User Profile',
         component: ProfileHome,
         permission: 3,
     },
     {
-        path: "/home/table",
+        path: '/home/table',
         exact: true,
-        icon: "table",
-        sidebarName: "Table List",
+        icon: 'table',
+        sidebarName: 'Table List',
         component: TableHome,
         permission: 2,
     },
     {
-        path: "/home/charts",
+        path: '/home/charts',
         exact: true,
-        icon: "charts",
-        sidebarName: "Charts",
+        icon: 'charts',
+        sidebarName: 'Charts',
         component: ChartsHome,
         permission: 1,
     },
     {
-        path: "/home/notice",
+        path: '/home/notice',
         exact: true,
-        icon: "notice",
-        sidebarName: "Notifications",
+        icon: 'notice',
+        sidebarName: 'Notifications',
         component: NoticeHome,
         permission: 1,
     },
