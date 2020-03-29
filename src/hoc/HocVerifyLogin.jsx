aafde3c7c637eb5b3ae9f7d51a61c48c27645a63
JianFei.Wang <jiucheng0605@gmail.com>
2020-03-29 11:27:17 +0800

    添加HocVerifyLogin的使用注释

--- src/hoc/HocVerifyLogin.jsx
+++ src/hoc/HocVerifyLogin.jsx
@@ -3,6 +3,12 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import { getLoginState } from '../store/modules/account/selector'
 
+/**
+ * 验证登录状态的的高阶组件
+ * 1.登录成功后setLogin把登录状态暂存
+ * 2.每次路由切换都会进入这个高阶组件进行验证登录是否过期，先获取getLoginState，融合到HocVerifyLogin，
+ *  若过期返回登录页面(类式vue路由的before验证)
+ */
 export default WrappedComponent => {
   const mapStateToProps = state => ({
     isLogin: getLoginState(state),
