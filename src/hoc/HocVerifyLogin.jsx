ee209852172d312b00f7a1ed0b822d7fb5ba0808
JianFei.Wang <jiucheng0605@gmail.com>
2020-03-29 11:31:23 +0800

    docs(添加hocverifylogin高阶组件的使用说明): 添加hocverifylogin高阶组件的使用说明
    添加hocverifylogin高阶组件的使用说明

--- src/hoc/HocVerifyLogin.jsx
+++ src/hoc/HocVerifyLogin.jsx
@@ -8,6 +8,7 @@ import { getLoginState } from '../store/modules/account/selector'
  * 1.登录成功后setLogin把登录状态暂存
  * 2.每次路由切换都会进入这个高阶组件进行验证登录是否过期，先获取getLoginState，融合到HocVerifyLogin，
  *  若过期返回登录页面(类式vue路由的before验证)
+ * 3.严格验证
  */
 export default WrappedComponent => {
   const mapStateToProps = state => ({
