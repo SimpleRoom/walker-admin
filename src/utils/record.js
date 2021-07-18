13b4ca36e3e3406b904fc82794c4aaaed2c39f5e
JianFei.Wang <jiucheng0605@gmail.com>
2021-07-18 21:37:36 +0800

    feat: 🎸 update record
    update

--- src/utils/record.js
+++ src/utils/record.js
@@ -1,23 +1,37 @@
-
-// 浏览器记录版本和构建时间
+// add record
 const { NODE_ENV } = process.env
-// console.log('%cEnvironment %cproduction', 'padding: 1px 2px; border-radius: 3px 0 0 3px; color: #fff;background:#606060;', 'padding: 1px 2px;background:#2072b5;')
-const customConsole = (key, value, bgColor) => {
-  console.log(`%c${key} %c${value}`, 'padding: 1px 2px; border-radius: 3px 0 0 3px; color: #fff;background:#606060;', `padding: 1px 2px;background:${bgColor || ''};`)
+const customConsole = (config) => {
+  console.log(
+    `%c${config.title} %c${config.value}`,
+    'padding: 1px 0 1px 4px; border-radius: 3px 0 0 3px; color: #fff;background:#606060;',
+    `border-radius: 0 3px 3px 0;padding: 1px 4px;background:${config.bgColor || ''};`
+  )
 }
+
 const formatDate = (times) => {
   const now = times ? new Date(times) : new Date()
-  var year = now.getFullYear()
-  var month = now.getMonth() + 1
-  var date = now.getDate()
-  var hour = now.getHours()
-  var minute = now.getMinutes()
-  var second = now.getSeconds()
-  return year + '-' + month + '-' + date + ' ' + hour + ':' + minute + ':' + second
+  const year = now.getFullYear()
+  const month = now.getMonth() + 1
+  const day = now.getDate()
+  const hour = now.getHours()
+  const minute = now.getMinutes()
+  const second = now.getSeconds()
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`
 }
-
 // eslint-disable-next-line no-undef
 const buildTime = formatDate(timeStamp)
-customConsole('Environment', NODE_ENV, '#1a9240')
-customConsole('Build Date', buildTime, '#2072b5')
-customConsole('Contact Us', 'https://github.com/jiucheng-front', 'transparent')
+customConsole({
+  title: 'Environment',
+  value: NODE_ENV,
+  bgColor: '#67c23a'
+})
+customConsole({
+  title: 'Build  Date',
+  value: buildTime,
+  bgColor: '#61dafb'
+})
+customConsole({
+  title: 'Contact  Us',
+  value: 'https://gitee.com/jikey/react-store-admin/',
+  bgColor: 'transparent'
+})
